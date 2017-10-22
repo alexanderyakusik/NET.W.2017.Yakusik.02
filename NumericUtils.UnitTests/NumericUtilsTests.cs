a5806b036ec6b275883254834d91749ed2b074ac
Alexander Yakusik <alexander.yakusik@gmail.com>
2017-10-22 11:19:55 +0300

    Implemented FindNextBiggerNumber method and created tests for it.

--- NumericUtils.UnitTests/NumericUtilsTests.cs
+++ NumericUtils.UnitTests/NumericUtilsTests.cs
@@ -1,10 +1,56 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
 
 namespace NumericUtils.NUnitTests
 {
     [TestFixture]
     public class NumericUtilsTests
     {
-        
+        [TestCase(12, 21)]
+        [TestCase(513, 531)]
+        [TestCase(2017, 2071)]
+        [TestCase(414, 441)]
+        [TestCase(144, 414)]
+        [TestCase(1234321, 1241233)]
+        [TestCase(1234126, 1234162)]
+        [TestCase(3456432, 3462345)]
+        [TestCase(10, -1)]
+        [TestCase(20, -1)]
+        public void FindNextBiggerNumber_CorrectNumberPassed_WorksCorrectly(int initialNumber, int expected)
+        {
+            int actual = NumericUtils.FindNextBiggerNumber(initialNumber);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(Int32.MinValue)]
+        public void FindNextBiggerNumber_NonPositiveNumberPassed_ArgumentOutOfRangeExceptionThrown(int initialNumber)
+        {
+            Assert.Catch<ArgumentOutOfRangeException>(() => NumericUtils.FindNextBiggerNumber(initialNumber));
+        }
+
+        [TestCase(Int32.MaxValue)]
+        [TestCase(1333333332)]
+        public void FindNextBiggerNumber_ReturnNumberIsBiggerThanHighestInteger_OverflowExceptionThrown(int initialNumber)
+        {
+            Assert.Catch<OverflowException>(() => NumericUtils.FindNextBiggerNumber(initialNumber));
+        }
+
+        [TestCase(1)]
+        [TestCase(123456789)]
+        [TestCase(198765432)]
+        public void FindNextBiggerNumber_CorrectNumberPassed_ExecutionTimeIsCorrect(int initialNumber)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            NumericUtils.FindNextBiggerNumber(initialNumber, out long actualMilliseconds);
+            sw.Stop();
+
+            Assert.AreEqual(sw.ElapsedMilliseconds, actualMilliseconds);
+        }
     }
 }
--- NumericUtils/NumericUtils.cs
+++ NumericUtils/NumericUtils.cs
@@ -1,18 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace NumericUtils
 {
     public static class NumericUtils
     {
-        public static void InsertNumber(int sourceNumber, ref int destinationNumber, int highBitIndex, int lowBitIndex)
+        public static int InsertNumber(int destinationNumber, int sourceNumber, int highBitIndex, int lowBitIndex)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Finds the nearest number greater than the input <paramref name="number"/> parameter,
+        /// consisting of the same digits.
+        /// </summary>
+        /// <param name="number">Input number</param>
+        /// <returns>Nearest number greater than the input <paramref name="number"/> parameter,
+        /// consisting of the same digits</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="number"/> is non positive.</exception>
+        /// <exception cref="OverflowException">Return value is not in the range of System.Int32 type.</exception>
         public static int FindNextBiggerNumber(int number)
         {
-            throw new NotImplementedException();
+            ValidatePositiveNumber(number);
+
+            if (number < 10)
+            {
+                return -1;
+            }
+
+            List<int> digits = SplitNumberIntoDigits(number);
+
+            FindFirstDigitGreaterThanTheNext(digits, out int currentIndex);
+
+            if (currentIndex + 1 == digits.Count)
+            {
+                return -1;
+            }
+
+            digits.Swap(currentIndex, currentIndex + 1);
+            digits.Sort(0, currentIndex + 1, Comparer<int>.Create((x, y) => x < y ? 1 : (x == y ? 0 : -1)));
+            digits.Reverse();
+
+            int result = GetNumberFromDigits(digits);
+            if (result <= 0)
+            {
+                throw new OverflowException();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Finds the nearest number greater than the input <paramref name="number"/> parameter,
+        /// consisting of the same digits.
+        /// </summary>
+        /// <param name="number">Input number</param>
+        /// <param name="elapsedMilliseconds">Method time execution</param>
+        /// <returns>Nearest number greater than the input <paramref name="number"/> parameter,
+        /// consisting of the same digits</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="number"/> is non positive.</exception>
+        /// <exception cref="OverflowException">Return value is not in the range of System.Int32 type.</exception>
+        public static int FindNextBiggerNumber(int number, out long elapsedMilliseconds)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            int result = FindNextBiggerNumber(number);
+            sw.Stop();
+
+            elapsedMilliseconds = sw.ElapsedMilliseconds;
+
+            return number;
+        }
+
+        private static void ValidatePositiveNumber(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static void FindFirstDigitGreaterThanTheNext(IList<int> list, out int digitIndex)
+        {
+            digitIndex = 0;
+            while (digitIndex + 1 < list.Count && list[digitIndex] <= list[digitIndex + 1])
+            {
+                digitIndex++;
+            }
+        }
+
+        private static List<int> SplitNumberIntoDigits(int number)
+        {
+            List<int> digits = new List<int>();
+
+            while (number > 0)
+            {
+                digits.Add(number % 10);
+                number /= 10;
+            }
+
+            return digits;
+        }
+
+        private static int GetNumberFromDigits(IList<int> digits)
+        {
+            int number = 0, multiplier = 1;
+
+            for (int i = digits.Count - 1; i >= 0; i--)
+            {
+                number += multiplier * digits[i];
+                multiplier *= 10;
+            }
+
+            return number;
+        }
+
+        private static void Swap<T>(this List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
         }
 
         public static void FilterDigit(IList<int> list, int digit)
